3fa7132fae467b653636a0a773f374d9f5a536c4
“rgulia” <rohitsingh_gulia@mcafee.com>
2021-11-01 17:52:38 -0400

    feature: update of overall website

--- src/App.js
+++ src/App.js
@@ -1,60 +1,15 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
-import { Switch, Route } from 'react-router-dom';
-import bio from './Pages/Bio/Bio';
-import interests from './Pages/Interests/Interests';
-import {HashRouter} from 'react-router-dom';
-import Paper from '@material-ui/core/Paper';
 import PropTypes from 'prop-types';
-import { withStyles } from '@material-ui/core/styles';
-import MenuAppBar from './Pages/AppMenu/MenuAppBar';
-import { BrowserRouter } from 'react-router-dom';
-import { ParallaxProvider } from 'react-scroll-parallax';
-import { MuiThemeProvider, createMuiTheme } from '@material-ui/core/styles';
-import purple from '@material-ui/core/colors/purple';
-import green from '@material-ui/core/colors/green';
-import { lightBlue } from '@material-ui/core/colors';
-
-const theme = createMuiTheme({
-  palette: {
-    primary: lightBlue,
-    secondary: green,
-    type: 'dark'
-  },
-  typography: { useNextVariants: true },
-});
-
-const styles = theme => ({
-    root: {
-        ...theme.mixins.gutters(),
-        paddingTop: theme.spacing.unit * 2,
-        paddingBottom: theme.spacing.unit * 2
-    },
-});
-
-// const theme = createMuiTheme({
-//   palette: {
-//     primary: purple,
-//     secondary: green,
-//   },
-//   status: {
-//     danger: 'orange',
-//   },
-// });
+import Main from './Pages/main';
 
 class App extends Component {
   render() {
       const { classes } = this.props;
     return (
-      <div className={classes.root}>
-          <HashRouter>
-              <ParallaxProvider>
-                <MuiThemeProvider theme={theme}>
-                  <MenuAppBar />
-                </MuiThemeProvider>
-              </ParallaxProvider>
-          </HashRouter>
+      <div>
+        <Main />
       </div>
     );
   }
@@ -64,4 +19,4 @@ App.propTypes = {
     classes: PropTypes.object.isRequired,
 };
 
-export default withStyles(styles)(App);
+export default App;
---
+++ src/Pages/main.js
@@ -0,0 +1,146 @@
+import * as React from 'react';
+import CssBaseline from '@mui/material/CssBaseline';
+import Box from '@mui/material/Box';
+import { Container } from '@mui/material';
+import Grid from '@mui/material/Grid';
+import { Button } from 'semantic-ui-react';
+import Typography from '@mui/material/Typography';
+import { Image, Reveal, Label, Segment, Grid as SemanticGrid, Divider, List} from 'semantic-ui-react';
+import Avatar from '@mui/material/Avatar';
+
+export default function SimpleContainer() {
+  return (
+    <React.Fragment>
+      <CssBaseline />
+      <Container maxWidth="md">
+
+          {/* content */}
+          <Grid className="content">
+                <Grid container textAlign= 'center'>
+                    <Grid item xs={12}>
+                        <Typography className={'headerFont'} variant="h1" component="h2"> R.S.G </Typography>
+                    </Grid>
+                </Grid>
+                <Divider hidden />
+                <Divider hidden />
+
+                <SemanticGrid columns={1}>
+                    <SemanticGrid.Column>
+                    <Segment raised>
+                        <Label as='a' color='red' ribbon>
+                            In-Office
+                        </Label>
+
+                        <Divider hidden />
+
+                        <SemanticGrid.Column>
+                            <Segment padded>
+                            <Label attached='top'>McAfee</Label>
+                            <List bulleted>
+                                <List.Item>
+                                    Working on creating new shared middleware for various Modile, Web and Desktop applications at McAfee.
+                                </List.Item>
+                                <List.Item>
+                                    Handling initiative to improve software development processes in the project.
+                                </List.Item>
+                            </List>
+                            </Segment>
+                        </SemanticGrid.Column>
+
+                        <Divider hidden />
+
+                        <SemanticGrid.Column>
+                            <Segment padded>
+                            <Label attached='top'>Vivint Solar - A Sunrun Company</Label>
+                            <List bulleted>
+                                <List.Item>
+                                    Created new features for IOT, Mobile and Web applications using Node JS, AWS, GCP, React, React Native,
+                                    Graph QL and various other tech
+                                </List.Item>
+                                <List.Item>
+                                    Worked on E-commerce platform to create payment gateway using Paypal.
+                                </List.Item>
+                                <List.Item>
+                                    Architecting and implementing solutions for grid services project using cloud computing.
+                                </List.Item>
+                                <List.Item>
+                                    Reviewing project specifications and designing technology solutions that meet or exceed performance expectations.
+                                </List.Item>
+                            </List>
+                            </Segment>
+                        </SemanticGrid.Column>
+
+                        <Divider hidden />
+
+                        <SemanticGrid.Column>
+                            <Segment padded>
+                            <Label attached='top'>Limosys Software</Label>
+                            <List bulleted>
+                                <List.Item>
+                                    Developed new concepts and redesigned web applications while collaborating with clients.
+                                </List.Item>
+                                <List.Item>
+                                    Managed development milestones from initial steps through final delivery.
+                                </List.Item>
+                                <List.Item>
+                                    Created web application for MTA to manage customer reservation and track fleet real-time.
+                                    Managing and supporting developed system and continuously upgrading application.
+                                </List.Item>
+                                <List.Item>
+                                    Reviewing project specifications and designing technology solutions that meet or exceed performance expectations.
+                                </List.Item>
+                            </List>
+                            </Segment>
+                        </SemanticGrid.Column>
+
+                    </Segment>
+                    </SemanticGrid.Column>
+
+                    <SemanticGrid.Column>
+                    <Segment raised>
+                        <Label as='a' color='teal' ribbon='right'>
+                            Outside-Office
+                        </Label>
+
+                        <Divider hidden />
+
+                        <SemanticGrid.Column>
+                            <Segment padded>
+                            <Label attached='top'>Fitness</Label>
+                            <List bulleted>
+                                <List.Item>
+                                    Believe in staying active whether its by running or going to gym or hiking.
+                                </List.Item>
+                                <List.Item>
+                                    P2 paragliding pilot. Out of tocuh for a while though since Toronto doesn't have that many mountains to fly from.
+                                </List.Item>
+                                <List.Item>
+                                    Like to eat clean and whole foods.
+                                </List.Item>
+                            </List>
+                            </Segment>
+                        </SemanticGrid.Column>
+                    </Segment>
+                    </SemanticGrid.Column>
+                </SemanticGrid>
+
+        </Grid>
+
+        <Divider hidden />
+
+        {/* footer */}
+        <Grid container textAlign= 'center' className="footer">
+            <Grid item xs={4}>
+                <Button circular color='facebook' icon='facebook' href={"https://www.facebook.com/rohit.gulia.54"} target="_blank" size={'massive'}/>
+            </Grid>
+            <Grid item xs={4}>
+                <Button circular color='linkedin' icon='linkedin' href={"https://www.linkedin.com/in/rohitgulia"} target="_blank" size={'massive'}/>
+            </Grid>
+            <Grid item xs={4}>
+                <Button circular color='github' icon='github' href={"https://github.com/rohitgulia"} target="_blank" size={'massive'}/>
+            </Grid>
+        </Grid>
+      </Container>
+    </React.Fragment>
+  );
+}
